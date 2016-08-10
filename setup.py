be7cd9f0397fb2bb618e0a4c077d2c932fd0121c
Isman Firmansyah <isman.firmansyah@gmail.com>
2016-08-11 03:05:23 +0700

    update version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
         name="Gluu-WebUI",
-        version="0.3",
+        version="0.5.1",
         url="https://github.com/GluuFederation/gluu-webui",
         license="MIT",
         author="Gluu",
